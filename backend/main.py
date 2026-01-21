9545d8749c2ba44de9e65c056fc0f6ffaa653fd8
Anshika <anshika.22b0151028@abes.ac.in>
2026-01-21 23:38:02 +0530

    Added the api routers and fixed few bugs

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
+from routers import story, job
+from db.database import create_tables
+
+create_tables()
 
 app = FastAPI(
     title = "Choose Your Own Adventure Game API",
@@ -13,11 +17,15 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=settings.ALLOWED_ORIGINS,  #[https://localhost:3000,https://localhost:5173]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(story.router, prefix=settings.API_PREFIX)
+
+app.include_router(job.router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn
